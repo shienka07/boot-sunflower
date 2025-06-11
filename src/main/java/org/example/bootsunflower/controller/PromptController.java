20974d740a7ad2c38fbe2708c2b50d42dcb479d3
JO KYEONGHYE <shienka07.ko@gmail.com>
2025-06-11 11:31:37 +0900

    get

--- src/main/java/org/example/bootsunflower/controller/PromptController.java
+++ src/main/java/org/example/bootsunflower/controller/PromptController.java
@@ -1,15 +1,20 @@
 package org.example.bootsunflower.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.bootsunflower.dto.PromptForm;
 import org.example.bootsunflower.service.PromptService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class PromptController {
     private final PromptService promptService;
 
-    public String index() {
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("promptForm", new PromptForm(""));
         return "index";
     }
 }
