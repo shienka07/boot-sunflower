c60bcb354084d26881d70f434ea3d257488fedfd
JO KYEONGHYE <shienka07.ko@gmail.com>
2025-06-13 11:13:53 +0900

    Entity & Repo

--- src/main/java/org/example/bootsunflower/entity/KakaoUser.java
+++ src/main/java/org/example/bootsunflower/entity/KakaoUser.java
@@ -1,4 +1,18 @@
 package org.example.bootsunflower.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class KakaoUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String username;
+    private String name;
+    private String profileImage;
 }
--- src/main/java/org/example/bootsunflower/repository/KakaoUserRepository.java
+++ src/main/java/org/example/bootsunflower/repository/KakaoUserRepository.java
@@ -1,4 +1,8 @@
 package org.example.bootsunflower.repository;
 
-public interface KakaoUserRepository {
+import org.example.bootsunflower.entity.KakaoUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KakaoUserRepository extends JpaRepository<KakaoUser, String> {
+    KakaoUser findByUsername(String username);
 }
